2b0336b6e3b571ca6646aef09ad8e0b9593d9987
KhanhTam540 <khanhtam540@>
2026-03-12 01:26:09 +0700

    WebCarParts

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -10,10 +10,6 @@ const pool = mysql.createPool({
     database: process.env.DB_NAME || 'defaultdb',
     port: process.env.DB_PORT || 26150,
     ssl: {
-        // Đọc file cert từ thư mục certs hoặc dùng cấu hình từ Render
-        ca: process.env.DB_SSL_CA || (fs.existsSync(path.join(__dirname, '../../certs/ca.pem')) 
-            ? fs.readFileSync(path.join(__dirname, '../../certs/ca.pem')) 
-            : undefined),
         rejectUnauthorized: false
     },
     waitForConnections: true,
